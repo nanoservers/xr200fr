842ec9686e201a9f29c48d0cd6c55a456fd53f5b
voltan <djvoltan@gmail.com>
2013-06-26 23:07:43 +0430

    Add english language for version 2.5.6

---
+++ language/en/core/2.5.6/install/language/english/support.php
@@ -0,0 +1,22 @@
+<?php
+// $Id: support.php 8066 2011-11-06 05:09:33Z beckmi $
+// _LANGCODE: en
+// _CHARSET : UTF-8
+// Translator: XOOPS Translation Team
+
+$supports = array();
+
+// Support site for English
+$supports["english"] = array(
+    "url"   => "http://www.xoops.org/",
+    "title" => "English support",
+);
+
+// Add extra support sites, use corresponding language folder name as key, for instance:
+/*
+$supports["french"] = array(
+    "url"   => "http://www.frxoops.org/",
+    "title" => "Support francophone"
+);
+*/
+?>
